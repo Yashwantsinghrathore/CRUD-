44010982ebb5317b4449b1e1755dc6e4f631097d
Yashwantsinghrathore <99130654+Yashwantsinghrathore@users.noreply.github.com>
2022-02-06 20:22:57 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		 <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>CRUD</title>
+		<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">	
+		<link rel="stylesheet" href="style.css"; />
+</head>
+	
+<body>
+	<div class="container">
+		<div class="showcase">
+			<h1>Crud Operation System</h1>
+			<div class="links">
+				<a href="index.php">Home</a>
+				<a href="add.php">Add</a>
+			</div>
+		</div>
+	</div>
+</body>
+		
+	
